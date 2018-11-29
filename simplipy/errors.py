009da69c10ae9914ff5c86e0ae5d7b93fdada624
jupiter <jupiter.ks0102@outlook.com>
2018-11-29 09:18:12 -0700

    Added pylintrc

--- simplipy/errors.py
+++ simplipy/errors.py
@@ -4,10 +4,16 @@
 class SimplipyError(Exception):
     """Define a base error."""
 
+    pass
+
 
 class InvalidCredentialsError(SimplipyError):
     """Define an error related to invalid requests."""
 
+    pass
+
 
 class RequestError(SimplipyError):
     """Define an error related to invalid requests."""
+
+    pass
