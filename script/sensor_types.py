7ef6e82b2ed9837873fc467abdb25c448f234b6d
jupiter <jupiter.ks0102@outlook.com>
2019-08-12 22:02:15 -0600

    Add script/ directory + script for finding sensors

---
+++ script/sensor_types.py
@@ -0,0 +1,43 @@
+"""Get a list of all sensors in a system."""
+import asyncio
+import logging
+import sys
+
+from simplipy import API
+from simplipy.errors import InvalidCredentialsError, SimplipyError
+
+sys.path.append(".")
+
+from aiohttp import ClientSession
+
+
+_LOGGER = logging.getLogger()
+
+SIMPLISAFE_EMAIL = "<EMAIL>"
+SIMPLISAFE_PASSWORD = "<PASSWORD>"
+
+
+async def main() -> None:
+    """Create the aiohttp session and run the example."""
+    async with ClientSession() as websession:
+        logging.basicConfig(level=logging.INFO)
+
+        try:
+            simplisafe = await API.login_via_credentials(
+                SIMPLISAFE_EMAIL, SIMPLISAFE_PASSWORD, websession
+            )
+            systems = await simplisafe.get_systems()
+            for system in systems.values():
+                for sensor_attrs in system.sensors.values():
+                    _LOGGER.info(
+                        "Sensor: (name: %s, type: %s)",
+                        sensor_attrs.name,
+                        sensor_attrs.type,
+                    )
+        except InvalidCredentialsError:
+            _LOGGER.error("Invalid credentials")
+        except SimplipyError as err:
+            _LOGGER.error(err)
+
+
+asyncio.get_event_loop().run_until_complete(main())
