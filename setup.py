17a7d5cd619eb71b721be6419d48be178057403a
jupiter <jupiter.ks0102@outlook.com>
2019-09-20 12:36:15 -0700

    Exclude scripts from published package (#46)

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ setup(
     # author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages(exclude=("tests",)),
+    packages=find_packages(exclude=("tests", "script")),
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['mypackage'],
     # entry_points={
