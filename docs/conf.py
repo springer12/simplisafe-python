6fe925e6f69dd454c6fee36f400eadac253ddbbb
jupiter <jupiter.ks0102@outlook.com>
2020-02-13 16:12:59 -0700

    Bump the docs version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2019, Aaron Bach"
 author = "Aaron Bach"
 
 # The full version, including alpha/beta/rc tags
-release = "5.2.0"
+release = "7.3.0"
 
 
 # -- General configuration ---------------------------------------------------
