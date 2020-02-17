41db0ad5eeeee3502a5712907ac53d84cc4d3764
jupiter <jupiter.ks0102@outlook.com>
2020-02-16 17:30:02 -0700

    Bump version to 8.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2019, Aaron Bach"
 author = "Aaron Bach"
 
 # The full version, including alpha/beta/rc tags
-release = "7.3.0"
+release = "8.0.0"
 
 
 # -- General configuration ---------------------------------------------------
